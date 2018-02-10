e91a7cb53ffdb1c7c668cb3bc9c660a7f61361ee
Miles Todzo <mt265@duke.edu>
2018-02-10 16:21:11 -0500

    Sugar Scape XML

---
+++ src/simulation/AntOccupant.java
@@ -0,0 +1,19 @@
+package simulation;
+
+import grids.Grid;
+import javafx.scene.paint.Paint;
+
+public class AntOccupant extends CellOccupant{
+
+	public AntOccupant(int initState, int[] initLocation, Paint initColor) {
+		super(initState, initLocation, initColor);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void calculateNextState(Grid grid) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
